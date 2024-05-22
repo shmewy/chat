e85755fd3ac88bf0f7747242cc5bdb3999522028
shmewy <167490786+shmewy@users.noreply.github.com>
2024-05-22 19:36:09 +0000

    jljlj

--- react-chat/src/firebase.js
+++ react-chat/src/firebase.js
@@ -3,15 +3,16 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
+import 'dotenv/config'; // Load environment variables from .env file
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCjvy5EKuRxrDyAkwDV9m0RToBLv4kwRec",
-    authDomain: "lunatechatapp.firebaseapp.com",
-    projectId: "lunatechatapp",
-    storageBucket: "lunatechatapp.appspot.com",
-    messagingSenderId: "272689538726",
-    appId: "1:272689538726:web:15f5be1bb794f8f735b667",
-    measurementId: "G-Y09M65WGJL"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 firebase.initializeApp(firebaseConfig);
